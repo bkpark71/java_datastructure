89b11d4583829c3023ae1c5fb7f8f73090a30ce1
bkpark <bokyoungp@naver.com>
2023-08-30 15:46:51 +0900

    java14일차 - 자료구조 v1.2

--- src/list/ArrList.java
+++ src/list/ArrList.java
@@ -28,15 +28,26 @@ public class ArrList<E> {
       a[size++] = e;
       return true;
     }
+
+    public E remove(){ // removeLast
+      if(size == 0)
+        throw new IndexOutOfBoundsException();
+      E tobeDeleted = a[size-1];
+      a[size-1] = null;
+      size--;
+      if(size > 0 && size == a.length/4){
+        resize(a.length / 2);
+      }
+      return tobeDeleted;
+    }
+
     
     private void resize(int newSize){
+      System.out.println("resize가 호출됨(" + newSize + ")");
       E[] t = (E[])new Object[newSize];
       for (int i = 0; i < size; i++) {
         t[i] = a[i];
       }
       a = t;
     }
-    
-
-
 }
--- src/list/ArrListTest.java
+++ src/list/ArrListTest.java
@@ -12,11 +12,22 @@ public class ArrListTest {
     System.out.println(list.get(1));
     list.add(30);
     list.add(40);
+    list.add(50);
+    System.out.println(list.remove() +"가 삭제됨");
+    System.out.println(list.remove() +"가 삭제됨");
+    System.out.println(list.remove() +"가 삭제됨");
+    System.out.println(list.remove() +"가 삭제됨");
+    System.out.println(list.remove() +"가 삭제됨");
     System.out.println(list.size());
 
     ArrayList<Integer> aList = new ArrayList<>();
     System.out.println(aList.size());
     aList.add(10);
+    aList.add(20);
+    aList.add(30);
+    aList.add(40);
+    aList.remove(aList.size()-1);
+    aList.remove(aList.size()-1);
     System.out.println(aList.get(0));
     System.out.println(aList.size());
   }
